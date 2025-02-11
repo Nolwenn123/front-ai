cff05b958d26eacabbd1646592a3aeb1233131d2
Nolwenn123 <nolwenn.bourey@gmail.com>
2025-02-11 10:37:19 +0100

    oups

--- src/main.py
+++ src/main.py
@@ -2,16 +2,15 @@ import pygame
 
 pygame.init()
 
-SCREEN_WIDTH = 800
+SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 800
 screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 pygame.display.set_caption(("Barman Game"))
 
-background = pygame.image.load("../assets/background.jpg")
-barman = pygame.image.load("../assets/barman.jpg")
 
-barman_x = SCREEN_WIDTH // 2 - 100
-barman_y = SCREEN_HEIGHT - 350
+
+background_barman = pygame.image.load("../assets/background-barman.png")
+background_barman = pygame.transform.scale(background_barman, (SCREEN_WIDTH, SCREEN_HEIGHT))
 
 # boucle du jeu
 running = True
@@ -21,9 +20,7 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    screen.blit(background, (0, 0))  # Dessiner le background en premier
-    screen.blit(barman, (barman_x, barman_y))  # Dessiner le barman par-dessus
-
+    screen.blit(background_barman, (0,0))
     # Mettre à jour l'écran
     pygame.display.flip()
 
